8999af1ff2afc3ca67631d931bf7498950a82191
KHAMIDJONOV RAKHMATULLO <khamidjonovrakhmatullo@KHAMIDJONOVs-MacBook-Air.local>
2024-09-03 18:54:15 +0900

    types

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
 "use strict";
-console.log(true + 10);
+// console.log("Bismillah");
+// throw new Error ("Some Error")
+// let a = 12
+// a = "str"  x
+// a = true  x
+// a = {}  x
+// a = 12 + 12
+let a = 33; // 23, 1.0021
+a = 11;
+// let b: string;
+// b = true;  x
+// b = 43;  x
+let b = "str"; // "", '', ``
+b = "text";
+let c = true; // true or false
+// c = function() {} x
+c = true;
+// let d = null
+let d = null; // null
+// d = undefined  x
+// let e = undefined
+let e = undefined; //undefined
+// let f = {}
+// let f : object = {}
+// f = 12  xx
+// f = {name : "Me"}
+// f.
+let f = { name: "Me" };
+// f.name = "You"
+let g;
+g = 12;
+g = "str";
+g = true;
+g = { name: "Me" };
+g = function () { };
+g = [];
+g = null;
+g = undefined;
 //# sourceMappingURL=index.js.map
